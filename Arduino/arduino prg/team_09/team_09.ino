da8f8f22840963331f4edbe5434a5f9885056993
bigcocks <45554997+bigcocks@users.noreply.github.com>
2018-12-03 16:48:53 +0800

    commit all

---
+++ Arduino/arduino prg/team_09/team_09.ino
@@ -0,0 +1,14 @@
+#define blueLed 13
+#define redLed 12
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(blueLed,OUTPUT)； //set mode for output
+    pinMode(redLed.OUTPUT)； //set mode for output
+    digitalWrite(blueLed,LOW)；          //set mode for default value
+    digitalWrite(redLed,LOW)；  //set mode for default value
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
